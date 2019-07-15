8ec4fb43a362e05ce66de124fd214525b50d75c2
Diksha Chauhan <dikshachauhan008@gmail.com>
2019-07-15 12:53:34 +1000

    galleryApp

--- acart_thymeleaf/src/main/java/com/Ibt/acart/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Ibt.acart.repository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.Ibt.acart.model.Address;
-@Repository
-public interface AddressRepository extends CrudRepository<Address, Long> {
-
-}
